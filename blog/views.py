5d6db87850e74fbd19c2a7d16c63ba95f6b23a47
eusalazar <eugeniasalazar745@gmail.com>
2024-03-12 11:02:00 -0300

    forms

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 # blog/views.py
 from django.views.generic import ListView, DetailView # new
+from django.views.generic.edit import CreateView, UpdateView, DeleteView # new
+from django.urls import reverse_lazy # new
+
+
 from .models import Post
 
 class BlogListView(ListView):
@@ -9,3 +13,18 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView): # new 
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView): # new 
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView): # new 
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView): # new 
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
